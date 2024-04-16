aa22a0041ac034a4e6ca95fda9c36c22313bc0b0
Aryan Dwivedi <aryan.735921@gmail.comm>
2024-04-16 15:43:49 +0530

    ✈registration and login apis are done

--- src/main/java/com/voyage/voyageapi/security/SecurityConfig.java
+++ src/main/java/com/voyage/voyageapi/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.voyage.voyageapi.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
